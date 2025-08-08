eeb5ca953a48cddde8aac5b476e93703a11bf529
Anton Shevchenko <Shevchenko-SPb@yandex.ru>
2025-08-08 11:12:25 +0300

    Change logic find Email in DB

--- home/bitrix/www/local/custom_app/import_contacts/Settings.php
+++ home/bitrix/www/local/custom_app/import_contacts/Settings.php
@@ -2,13 +2,16 @@
 
 class Settings
 {
-    // Пользовательское поле для хранения ИНН компании в контакте.
-    // Необходимо для исключения дубликатов при повторном импорте,
-    // если у контакта нет телефона и Email
-    public const UF_CONTACT_INN_COMPANY = 'UF_CRM_1753953016'; // [AT] ИНН Компании
-    public const UF_COMPANY_COUNTRY = 'UF_CRM_1753805102'; // Страна
+    // Пользовательское поля
+    public const UF_CONTACT_INN_COMPANY = 'UF_CRM_1753953016'; // [AT] ИНН Компании // string
+    public const UF_CONTACT_COUNTRY = 'UF_CRM_1754548956'; // [AT] Страна // string
+    public const UF_CONTACT_FED_REGION = 'UF_CRM_1754549070'; // [AT] РФ (Фед округ) // string
+    public const UF_CONTACT_WORK_PHONE = 'UF_CRM_1754549124'; // [AT] Телефон (рабочий) // string
+    public const UF_CONTACT_MOBILE_PHONE = 'UF_CRM_1754549146'; // [AT] Телефон (мобильный) // string
+    public const UF_COMPANY_COUNTRY = 'UF_CRM_1753805102'; // Страна // Список
+    public const UF_COMPANY_FED_REGION = 'UF_CRM_FEDERAL_DISTRICT_ID'; // РФ (Фед округ) // Привязка к элементам инфоблоков
 
-    // Этот порядок используется в UploadContacts;
+    // Этот порядок используется в UploadContacts для проверки очерёдности столбцов;
     public const AR_FIELDS = [
         'Контрагент', // Наименование полное название Компании
         'ИНН', // ИНН компании (обязательное поле)
---
+++ home/bitrix/www/local/custom_app/import_contacts/test.php
@@ -0,0 +1,24 @@
+<?php
+//require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+//require_once('./Settings.php');
+//require_once('./lib/UploadContacts.php');
+//
+//$data = [
+//    [
+//        'ООО "Монолюм"', // Контрагент
+//        '7802791649', // ИНН
+//        'Россия', // Страна
+//        "Олег  Кельнер", // ФИО
+//        "", // Роль
+//        "", // Телефон
+//        "", // Мобильный
+//        "olegkelner08+fti@gmail.com" // Имейл
+//    ]
+//];
+//
+//$class = new UploadContacts();
+//echo '<pre>';
+//print_r($class->run($data));
+//echo '</pre>';
+
+//ООО "Монолюм"	7802791649	Россия	Олег  Кельнер				olegkelner08+fti@gmail.com
